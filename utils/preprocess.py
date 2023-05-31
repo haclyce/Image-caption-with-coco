9b5f7026b43458d18580e6bbddf0095966c53d70
Stanlumei <StanleyFCB@163.com>
2023-05-31 12:29:22 +0800

    edit dataloader

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,11 +4,182 @@
 # @Time     : 2023/5/30 23:23
 # @File     : preprocess.py
 # @Project  : lab
+import json
 import os
+import pickle
 import random
+from collections import Counter
 
+import nltk
 import numpy as np
 import torch
+from PIL import Image
+from pycocotools.coco import COCO
+from skimage import io
+from torch.utils import data
+from tqdm import tqdm
+
+
+class Vocabulary(object):
+    def __init__(self,
+                 vocab_threshold,
+                 vocab_file='./data/vocab.pkl',
+                 start_word="<start>",
+                 end_word="<end>",
+                 unk_word="<unk>",
+                 annotations_file='./data/annotations_DCC/captions_no_caption_rm_eightCluster_train2014.json',
+                 vocab_from_file=False):
+        """Initialize the vocabulary.
+        Args:
+          vocab_threshold: Minimum word count threshold.
+          vocab_file: File containing the vocabulary.
+          start_word: Special word denoting sentence start.
+          end_word: Special word denoting sentence end.
+          unk_word: Special word denoting unknown words.
+          annotations_file: Path for train annotation file.
+          vocab_from_file: If False, create vocab from scratch & override any existing vocab_file
+                           If True, load vocab from existing vocab_file, if it exists
+        """
+        self.vocab_threshold = vocab_threshold
+        self.vocab_file = vocab_file
+        self.start_word = start_word
+        self.end_word = end_word
+        self.unk_word = unk_word
+        self.annotations_file = annotations_file
+        self.vocab_from_file = vocab_from_file
+        self.get_vocab()
+
+    def get_vocab(self):
+        """Load the vocabulary from file OR build the vocabulary from scratch."""
+        if os.path.exists(self.vocab_file) & self.vocab_from_file:
+            with open(self.vocab_file, 'rb') as f:
+                vocab = pickle.load(f)
+                self.word2idx = vocab.word2idx
+                self.idx2word = vocab.idx2word
+            print('Vocabulary successfully loaded from vocab.pkl file!')
+        else:
+            self.build_vocab()
+            with open(self.vocab_file, 'wb') as f:
+                pickle.dump(self, f)
+
+    def build_vocab(self):
+        """Populate the dictionaries for converting tokens to integers (and vice-versa)."""
+        self.init_vocab()
+        self.add_word(self.start_word)
+        self.add_word(self.end_word)
+        self.add_word(self.unk_word)
+        self.add_captions()
+
+    def init_vocab(self):
+        """Initialize the dictionaries for converting tokens to integers (and vice-versa)."""
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        """Add a token to the vocabulary."""
+        if word not in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def add_captions(self):
+        """Loop over training captions and add all tokens to the vocabulary that meet or exceed the threshold."""
+        coco = COCO(self.annotations_file)
+        counter = Counter()
+        ids = coco.anns.keys()
+        for i, ID in enumerate(ids):
+            caption = str(coco.anns[ID]['caption'])
+            tokens = nltk.tokenize.word_tokenize(caption.lower())
+            counter.update(tokens)
+
+            if i % 50000 == 0:
+                print("[%d/%d] Tokenizing captions..." % (i, len(ids)))
+
+        words = [word for word, cnt in counter.items() if cnt >= self.vocab_threshold]
+
+        for i, word in enumerate(words):
+            self.add_word(word)
+
+    def __call__(self, word):
+        if word not in self.word2idx:
+            return self.word2idx[self.unk_word]
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.word2idx)
+
+
+class CoCoDataset(data.Dataset):
+    def __init__(self, transform, mode, batch_size, vocab_threshold, vocab_file, start_word,
+                 end_word, unk_word, annotations_file, vocab_from_file):
+        self.transform = transform
+        self.mode = mode
+        self.batch_size = batch_size
+        self.vocab = Vocabulary(vocab_threshold, vocab_file, start_word,
+                                end_word, unk_word, annotations_file, vocab_from_file)
+        # self.img_folder = img_folder
+        if self.mode == 'train':
+            self.coco = COCO(annotations_file)
+            self.ids = list(self.coco.anns.keys())
+            print('Obtaining caption lengths...')
+            all_tokens = [nltk.tokenize.word_tokenize(str(self.coco.anns[self.ids[index]]['caption']).lower()) for index
+                          in tqdm(np.arange(len(self.ids)))]
+            self.caption_lengths = [len(token) for token in all_tokens]
+        else:
+            test_info = json.loads(open(annotations_file).read())
+            # self.paths = [item['file_name'] for item in test_info['images']]
+            self.paths = [item['url'] for item in test_info['images']]
+
+    def __getitem__(self, index):
+        # obtain image and caption if in training mode
+        if self.mode == 'train':
+            ann_id = self.ids[index]
+            caption = self.coco.anns[ann_id]['caption']
+            img_id = self.coco.anns[ann_id]['image_id']
+            # path = self.coco.loadImgs(img_id)[0]['file_name']
+            img = self.coco.loadImgs(img_id)[0]
+
+            # Convert image to tensor and pre-process using transform
+            # image = Image.open(os.path.join(self.img_folder, path)).convert('RGB')
+            # image = self.transform(image)
+            image = io.imread(img['url'])
+            image = self.transform(Image.fromarray(image))
+
+            # Convert caption to tensor of word ids.
+            tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+            caption = [self.vocab(self.vocab.start_word)]
+            caption.extend([self.vocab(token) for token in tokens])
+            caption.append(self.vocab(self.vocab.end_word))
+            caption = torch.Tensor(caption).long()
+
+            # return pre-processed image and caption tensors
+            return image, caption
+
+        # obtain image if in test mode
+        else:
+            path = self.paths[index]
+
+            # Convert image to tensor and pre-process using transform
+            # PIL_image = Image.open(os.path.join(self.img_folder, path)).convert('RGB')
+            # orig_image = np.array(PIL_image)
+            # image = self.transform(PIL_image)
+            orig_image = io.imread(path)
+            image = self.transform(Image.fromarray(orig_image))
+            # return original image and pre-processed image tensor
+            return orig_image, image
+
+    def get_train_indices(self):
+        sel_length = np.random.choice(self.caption_lengths)
+        all_indices = np.where([self.caption_lengths[i] == sel_length for i in np.arange(len(self.caption_lengths))])[0]
+        indices = list(np.random.choice(all_indices, size=self.batch_size))
+        return indices
+
+    def __len__(self):
+        if self.mode == 'train':
+            return len(self.ids)
+        else:
+            return len(self.paths)
 
 
 def reproduce(seed):
@@ -23,3 +194,78 @@ def reproduce(seed):
     if not os.path.isdir('./checkpoints'):
         os.mkdir('./checkpoints')
     return 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def get_loader(transform,
+               mode='train',
+               batch_size=1,
+               vocab_threshold=None,
+               vocab_file='./data/vocab.pkl',
+               start_word="<start>",
+               end_word="<end>",
+               unk_word="<unk>",
+               vocab_from_file=True,
+               num_workers=0,
+               cocoapi_loc='/opt'):
+    """Returns the data loader.
+    Args:
+      transform: Image transform.
+      mode: One of 'train' or 'test'.
+      batch_size: Batch size (if in testing mode, must have batch_size=1).
+      vocab_threshold: Minimum word count threshold.
+      vocab_file: File containing the vocabulary.
+      start_word: Special word denoting sentence start.
+      end_word: Special word denoting sentence end.
+      unk_word: Special word denoting unknown words.
+      vocab_from_file: If False, create vocab from scratch & override any existing vocab_file.
+                       If True, load vocab from existing vocab_file, if it exists.
+      num_workers: Number of subprocesses to use for data loading
+      cocoapi_loc: The location of the folder containing the COCO API: https://github.com/cocodataset/cocoapi
+    """
+
+    assert mode in ['train', 'test'], "mode must be one of 'train' or 'test'."
+    if not vocab_from_file:
+        assert mode == 'train', "To generate vocab from captions file, must be in training mode (mode='train')."
+
+    # Based on mode (train, val, test), obtain img_folder and annotations_file.
+    if mode == 'train':
+        if vocab_from_file:
+            assert os.path.exists(
+                vocab_file), "vocab_file does not exist.  Change vocab_from_file to False to create vocab_file."
+        # img_folder = os.path.join(cocoapi_loc, 'cocoapi/images/train2014/')
+        annotations_file = './data/annotations_DCC/captions_no_caption_rm_eightCluster_train2014.json'
+    if mode == 'test':
+        assert batch_size == 1, "Please change batch_size to 1 if testing your model."
+        assert os.path.exists(vocab_file), "Must first generate vocab.pkl from training data."
+        assert vocab_from_file, "Change vocab_from_file to True."
+        # img_folder = os.path.join(cocoapi_loc, 'cocoapi/images/test2014/')
+        annotations_file = os.path.join(cocoapi_loc, 'cocoapi/annotations/image_info_test2014.json')
+
+    # COCO caption dataset.
+    dataset = CoCoDataset(transform=transform,
+                          mode=mode,
+                          batch_size=batch_size,
+                          vocab_threshold=vocab_threshold,
+                          vocab_file=vocab_file,
+                          start_word=start_word,
+                          end_word=end_word,
+                          unk_word=unk_word,
+                          annotations_file=annotations_file,
+                          vocab_from_file=vocab_from_file)
+    if mode == 'train':
+        # Randomly sample a caption length, and sample indices with that length.
+        indices = dataset.get_train_indices()
+        # Create and assign a batch sampler to retrieve a batch with the sampled indices.
+        initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+        # data loader for COCO dataset.
+        data_loader = data.DataLoader(dataset=dataset,
+                                      num_workers=num_workers,
+                                      batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
+                                                                              batch_size=dataset.batch_size,
+                                                                              drop_last=False))
+    else:
+        data_loader = data.DataLoader(dataset=dataset,
+                                      batch_size=dataset.batch_size,
+                                      shuffle=True,
+                                      num_workers=num_workers)
+    return data_loader
